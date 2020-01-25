04f4fb554202d8c4760e873ffd97b50ab833570c
jisbruzzi <jose.sbru@gmail.com>
2020-01-25 16:38:54 -0300

    Agrego algunas gamerules como para intentar llegar al 2048

--- src/components/GameRules.js
+++ src/components/GameRules.js
@@ -1,23 +1,24 @@
-function boxAt(boxes,{x,y}){
-    return boxes.find(b=>b.x==x && b.y==y) || null;
-}
-function randomPosition(){
-    return {
-        x:Math.floor(Math.random()*4),
-        y:Math.floor(Math.random()*4),
+function addBox(boxes){
+    function boxAt(boxes,{x,y}){
+        return boxes.find(b=>b.x==x && b.y==y) || null;
     }
-}
-function boardIsFull(boxes){
-    for(let x=0;x<4;x++){
-        for(let y=0;y<4;y++){
-            if(!boxAt(boxes,{x,y})){
-                return false;
-            }
+    function randomPosition(){
+        return {
+            x:Math.floor(Math.random()*4),
+            y:Math.floor(Math.random()*4),
         }
     }
-    return true;
-}
-function addBox(boxes){
+    function boardIsFull(boxes){
+        for(let x=0;x<4;x++){
+            for(let y=0;y<4;y++){
+                if(!boxAt(boxes,{x,y})){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     if(boardIsFull(boxes)){
         return boxes;
     }
@@ -33,26 +34,22 @@ function addBox(boxes){
     }])
 }
 
-function movePosition(direction,pos){
-    switch(direction){
-        case "up":pos.y-=1;break;
-        case "down":pos.y+=1;break;
-        case "left":pos.x-=1;break;
-        case "right":pos.x+=1;break;
-    }
-    return pos;
-}
-function moveBox(direction,box){
-    let pos=movePosition(direction,{x:box.x,y:box.y});
-    return {
-        x:pos.x,
-        y:pos.y,
-        value:box.value,
-        id:box.id
-    };
-}
+
 function moveBoxes(direction,boxes){
-    return boxes.map(b=>moveBox(direction,b))
+    function boxesInAxis(z){
+
+    }
+
+
+    let ret=[]
+    for(let z=0;z<3;z+=1){
+        let current=boxesInAxis(z);
+        let projected=projectBoxescurrent);
+        let moved=moveBoxes(projected);
+        let unProjected=rebuildBoxes(z,moved);
+        ret=ret.concat(unProjected);
+    }
+    return ret;
 }
 
 export function move(direction,boxes){
