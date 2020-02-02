d2b77ec69351922ab5f428de7e193830deaf99c6
jisbruzzi <jose.sbru@gmail.com>
2020-02-01 21:28:48 -0300

    Now numbers dont always spawn

--- src/games/Classic.js
+++ src/games/Classic.js
@@ -37,76 +37,80 @@ function addBox(boxes){
 }
 
 
-function moveBoxes(direction,boxes){
-    function inAxis(b,z){
-        if(direction=="up" || direction=="down"){
-            return z==b.x;
-        }else{
-            return z==b.y;
-        }
+function project(box,direction){// movement towards 0
+    let z=0;
+    switch(direction){
+        case "up": z = box.y; break;
+        case "down": z = 3 - box.y; break;
+        case "left": z = box.x; break;
+        case "right": z = 3 - box.x; break;
     }
-    function project(box){// movement towards 0
-        let z=0;
-        switch(direction){
-            case "up": z = box.y; break;
-            case "down": z = 3 - box.y; break;
-            case "left": z = box.x; break;
-            case "right": z = 3 - box.x; break;
-        }
-        return {
-            z,
-            id:box.id,
-            value:box.value,
-            merged:false
-        };
+    return {
+        z,
+        id:box.id,
+        value:box.value,
+        merged:false
+    };
+}
+function inAxis(box,axis,direction){
+    if(direction=="up" || direction=="down"){
+        return axis==box.x;
+    }else{
+        return axis==box.y;
     }
-    function moveProjectedBoxes(boxes){
-        function boxAt(z){
-            return boxes.find(b=>b.z==z) || null
-        }
-        let orderedBoxes = []
-        function lastBox(){
-            if(orderedBoxes.length==0){
-                return null
-            }else{
-                return orderedBoxes[orderedBoxes.length-1];
-            }
-        }
-        function mergeBoxes(recent,previous){
-            previous.above=false
-            return {
-                z:previous.z,
-                value:recent.value*2,
-                id:recent.id,
-                above:true,
-                merged:true,
-            }
-        }
+}
 
-        console.log("entran:",boxes)
-        for(let z=0;z<4;z+=1){
-            let current = boxAt(z)
-            let last=lastBox()
-            if(current!=null){
-                if(last==null){
-                    orderedBoxes.push(current)
+
+function moveProjectedBoxes(boxes){
+    function boxAt(z){
+        return boxes.find(b=>b.z==z) || null
+    }
+    let orderedBoxes = []
+    function lastBox(){
+        if(orderedBoxes.length==0){
+            return null
+        }else{
+            return orderedBoxes[orderedBoxes.length-1];
+        }
+    }
+    function mergeBoxes(recent,previous){
+        previous.above=false
+        return {
+            z:previous.z,
+            value:recent.value*2,
+            id:recent.id,
+            above:true,
+            merged:true,
+        }
+    }
+
+    console.log("entran:",boxes)
+    for(let z=0;z<4;z+=1){
+        let current = boxAt(z)
+        let last=lastBox()
+        if(current!=null){
+            if(last==null){
+                orderedBoxes.push(current)
+            }else{
+                if(last.value==current.value && !last.merged){
+                    orderedBoxes.pop()
+                    orderedBoxes.push(mergeBoxes(current,last))
                 }else{
-                    if(last.value==current.value && !last.merged){
-                        orderedBoxes.pop()
-                        orderedBoxes.push(mergeBoxes(current,last))
-                    }else{
-                        orderedBoxes.push(current)
-                    }
+                    orderedBoxes.push(current)
                 }
             }
         }
-        let ret = orderedBoxes.map((box,index)=>{
-            box.z=index;
-            return box;
-        })
-        console.log("salen:",ret)
-        return ret;
     }
+    let ret = orderedBoxes.map((box,index)=>{
+        box.z=index;
+        return box;
+    })
+    console.log("salen:",ret)
+    return ret;
+}
+
+function moveBoxes(direction,boxes){
+
     function rebuildBox(z,box){
         let newBox={}
         switch(direction){
@@ -136,8 +140,8 @@ function moveBoxes(direction,boxes){
 
     let ret=[]
     for(let z=0;z<4;z+=1){
-        let current=boxes.filter(b=>inAxis(b,z))
-        let projected=current.map(project);
+        let current=boxes.filter(b=>inAxis(b,z,direction))
+        let projected=current.map((b)=>project(b,direction));
         let moved=moveProjectedBoxes(projected);
         let unProjected=moved.map(b=>rebuildBox(z,b));
         ret=ret.concat(unProjected);
@@ -145,7 +149,32 @@ function moveBoxes(direction,boxes){
     return ret;
 }
 
+function boxesMoved(moved,original){
+    function idDictionary(boxes){
+        let ret={}
+        console.log("=====",boxes)
+        for(let b of boxes){
+            ret[b.id]=b;
+        }
+        return ret;
+    }
+    let movedDict=idDictionary(moved);
+    let originalDict=idDictionary(original);
+    for(let id of Object.keys(movedDict)){
+        let movedBox = movedDict[id]
+        let originalBox = originalDict[id]
+        if(movedBox.x!=originalBox.x || movedBox.y!=originalBox.y){
+            return true;
+        }
+    }
+    return false;
+}
+
 export function move(direction,boxes){
-    let ret= addBox(moveBoxes(direction,boxes));
-    return ret;
+    let moved=moveBoxes(direction,boxes);
+    if(boxesMoved(moved,boxes) || boxes.length==0){
+        return addBox(moved);
+    }else{
+        return boxes;
+    }
 }
